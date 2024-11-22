c131390e082910b66e52e545d7778231d4f8c880
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-11-22 11:23:40 -0600

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
+        'cod_user',
         'password',
+        'role',
     ];
 
     /**
